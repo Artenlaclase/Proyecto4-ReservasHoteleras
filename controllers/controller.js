edda0261b050bce6019d1a0aa01d6e9592a1c7ba
Artenlaclase <artenlaclase@gmail.com>
2024-06-17 21:16:22 -0400

    Tipo de Habitacion

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,6 +5,7 @@ const { v4: uuidv4} = require("uuid");
 
 let bookings = [];
 
+// Crear una Reserva. POST /api/booking 
 exports.createBooking = async (req, res) => {
   const { nameHotel ,arrivalDate, departureDate, typeRoom, passengers, name, mail, paymentStatus } =
     req.body;
@@ -49,6 +50,7 @@ exports.createBooking = async (req, res) => {
   });
 };
 
+// Recupera una lista de todas las Reservas. GET /api/booking 
 exports.getBooking = async (req, res) => {
   
     return res.json({
@@ -58,6 +60,7 @@ exports.getBooking = async (req, res) => {
  
   };
 
+// Recupera los detalles de una reserva específica. GET /api/booking/:id 
 exports.getBookingById= async (req, res) => {
   const bookingId = req.params.id;
   const booking = bookings.find(booking => booking.id === bookingId );
@@ -70,6 +73,8 @@ exports.getBookingById= async (req, res) => {
     data: booking
 })
 }
+
+// Actualiza los detalles de una reserva existente. PUT /api/booking/:id 
 exports.upDateBookingById = async (req, res) => {
   const bookingId = req.params.id;
   const bookingIndex = bookings.findIndex(booking => booking.id === bookingId);
@@ -86,6 +91,7 @@ exports.upDateBookingById = async (req, res) => {
   })
 }
 
+// Elimina una reserva del sistema buscada desde el id. DELETE /api/booking/:id 
 exports.deleteBookingById = async (req, res) => {
   const bookingId = req.params.id;
   const bookingIndex = bookings.findIndex(booking => booking.id === bookingId);
@@ -94,18 +100,23 @@ exports.deleteBookingById = async (req, res) => {
       return res.status(404).json({ msg: "Reserva no encontrada." })
   }
 
- bookings.splice (bookingIndex,1)
+ bookings.splice(bookingIndex,1)
 
   return res.json({
       msg: 'Reserva eliminada con éxito.',
   })
 
-}
+};
 
 // Buesquedas segun distintas peticiones 
+// 01. "Como gerente de una cadena de hoteles, quiero ver todas las reservas para el "Hotel Paraíso" para el próximo mes."/api/reservas?hotel=HOTEL 
+// 02. "Como gerente del hotel, quiero ver todas las reservas para la semana de Navidad para poder planificar el personal y las actividades necesarias." /api/reservas?fecha_inicio=FECHA_INICIO&fecha_fin=FECHA_FIN
+// 03. "Como gerente del hotel, quiero ver todas las reservas para nuestras suites de lujo para el próximo mes para asegurarme de que todo esté en perfectas condiciones para nuestros huéspedes VIP." /api/reservas?tipo_habitacion=TIPO_HABITACION
+// 04. "Como gerente del hotel, quiero ver todas las reservas que están pendientes de pago para poder hacer un seguimiento con los clientes."/api/reservas?estado=ESTADO
+// 05. "Como gerente del hotel, quiero ver todas las reservas para grupos de más de 5 personas para el próximo mes, para poder planificar las necesidades adicionales de estos grupos grandes." /api/reservas?num_huespedes=NUM_HUESPEDES
 
-exports.getBooking = async (req, res) => {
-  const { arrivalDate, departureDate, typeRoom } = req.query;
+exports.getBookingInfo = async (req, res) => {
+  const { nameHotel, arrivalDate, departureDate, typeRoom, passengers, paymentStatus } = req.query;
 
   if (typeRoom) {
     const bookingFiltered = bookings.filter(
@@ -134,7 +145,7 @@ exports.getBooking = async (req, res) => {
         ) === true
     );
     if (usersFiltered.length === 0) {
-      return res.status(404).json({ msg: "No se encontraron usuarios" });
+      return res.status(404).json({ msg: "No se encontraron reservas" });
     }
     return res.json({
       msg: "Reservas",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/booking", bookingController.createBooking);
 router.get("/booking", bookingController.getBooking);
+router.get("/booking", bookingController.getBookingInfo);
 router.get("/booking/:id", bookingController.getBookingById);
 router.put("/booking/:id", bookingController.upDateBookingById);
 router.delete("booking/:id", bookingController.deleteBookingById);
